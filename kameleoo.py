551348d3a121550012a0c8d18a0efda31816335c
BeMoreHappY <domintokomin@gmail.com>
2023-04-05 17:49:30 +0200

    second commmit

--- kameleoo.py
+++ kameleoo.py
@@ -1,8 +1,12 @@
+import time
+
 from kameleo.local_api_client.kameleo_local_api_client import KameleoLocalApiClient
 from kameleo.local_api_client.builder_for_create_profile import BuilderForCreateProfile
 from kameleo.local_api_client.models.load_profile_request_py3 import LoadProfileRequest
 from kameleo.local_api_client.models.save_profile_request_py3 import SaveProfileRequest
 from kameleo.local_api_client.models.server_py3 import Server
+import json
+import io
 import os
 
 
@@ -12,42 +16,95 @@ class Kameleo:
         self.port = port
         self.client = KameleoLocalApiClient(f'http://localhost:{self.port}')
         self.base_profiles = self.client.search_base_profiles(
+            os_family="windows",
             device_type='desktop',
-            language="de"
+            language="de-DE"
         )
         self.create_profile_request = None
         self.profile = None
         self.path = None
         self.name = None
+        self.dictProfile = None
 
     def createProfile(self, name, ip, port):
         create_profile_request = BuilderForCreateProfile \
             .for_base_profile(self.base_profiles[0].id) \
-            .set_name(str(name)) \
+            .set_name(name) \
             .set_proxy('socks5', Server(host=ip, port=port)) \
             .set_recommended_defaults() \
             .build()
         self.profile = self.client.create_profile(body=create_profile_request)
         self.name = name
+        self.path = f"C:\\Users\\Mati\\Desktop\\{name}.kameleo"
+        print(self.profile, name)
+        print(f'Utworzono profil o ID - {self.profile.id}')
+        time.sleep(5)
+        self.saveInFile()
+
+    def readProfile(self):
+        self.client.list_profiles()
+    def AllProfile(self):
+        print(self.client.list_profiles())
 
     def setProfile(self, id):
         self.profile = self.client.read_profile(id)
+        print(f'Ustawiono profil o ID - {self.profile.id}')
 
-    def startProfile(self):
-        self.client.start_profile(self.profile.id)
+    def startProfile(self, id=None):
+        if id is None:
+            id = self.profile.id
+        self.client.start_profile(id)
+        print(f'Uruchomiono profil o ID - {id}')
 
     def stopProfile(self):
         self.client.stop_profile(self.profile.id)
+        print(f'Wstrzymano profil o ID - {self.profile.id}')
 
-    def saveProfile(self):
-        path = f'{os.path.dirname(os.path.realpath(__file__))}\\{self.name}'
+    def stopClient(self):
+        self.client.close()
+
+    def saveProfile(self, path=None):
+        if path is None:
+            path = self.path
         self.client.save_profile(self.profile.id, body=SaveProfileRequest(path=path))
+        print(f'Zapisano profil o ID - {self.profile.id}')
 
-    def loadProfile(self, name):
-        self.file = name
-        path = f'{os.path.dirname(os.path.realpath(__file__))}\\{self.file}'
-        print(path)
+    def loadProfile(self, path):
+        self.path = path
         self.profile = self.client.load_profile(body=LoadProfileRequest(path=path))
+        print(f'Załadowano profil o ID - {self.profile.id}')
 
     def deleteProfile(self):
         self.client.delete_profile(self.profile.id)
+        print(f'Usunięto profil o ID - {self.profile.id}')
+
+    def saveInFile(self):
+        try:
+            with open("profile.txt", "r") as f:
+                self.dictProfile = json.load(f)
+            with open("profile.txt", "w") as f:
+                if self.name not in self.dictProfile:
+                    self.dictProfile[self.name] = self.profile.id
+                    json.dump(self.dictProfile, f)
+                else:
+                    if self.dictProfile[self.name] != self.profile.id:
+                        print("ID nie są takie same")
+        except io.UnsupportedOperation:
+            self.dictProfile = {}
+            self.dictProfile[self.name] = self.profile.id
+            json.dump(self.dictProfile, f)
+        print("Zapisano ID profilu")
+
+    def checkIDprofileByName(self, name):
+        with open("profile.txt", "r") as f:
+            try:
+                self.dictProfile = json.load(f)
+                if name not in self.dictProfile:
+                    print("Nie ma takiego profilu o podanej nazwie")
+                    return False
+                return self.dictProfile[name]
+            except io.UnsupportedOperation:
+                print("Nie ma takiego profilu o podanej nazwie")
+                return False
+
+
--- main.py
+++ main.py
@@ -5,48 +5,69 @@ from selenium import webdriver
 import germanNameGenerator as namegen
 import time
 import requests
+from kameleo.local_api_client.models.problem_response_py3 import ProblemResponseException
 from conditions import WaitForElementPresence, PickEmail, WaitForElementClicable, CodeFromEmail, RemoveFirstLineFile
 
 
-try:
-    changeIP = False
-    kamelObj = Kameleo()
-    name = namegen.get_random_name()
-    #email = PickEmail("email.txt")
-    email = ["Matiskatibusdhw@gmail.com","sadoiv@ios12"]
-    #RemoveFirstLineFile("email.txt")
 
-    print(kamelObj.port)
-    passwd = f'{name}1234'
-    if changeIP:
-        proxy = requests.get('http://192.168.1.2:9049/v1/ips?num=1&country=DE&state=all&city=all&zip=all&t=txt&port=40000&isp=all&start=&end=').text.split(":")
-        IP = proxy[0]
-        port = int(proxy[1])
-    else:
-        IP = "192.168.1.2"
-        port = 30000
-    publicIP = requests.get("https://api.ipify.org").text
-    allData = [publicIP, email, name, passwd]
-    print(allData)
+changeIP = False
+metoda = 2 #    1 - start profil by profilName  2 - create profile
+profileName = "LauraSchulte"
 
-    kamelObj.setProfile("dfb7717e-4a76-40e8-a6b9-338ad04d76aa")
+
+kamelObj = Kameleo()
+firstName = str(namegen.get_random_name()).strip()
+lastName = str(namegen.get_random_familyname()).strip()
+fullName = f'{firstName} {lastName}'
+#email = PickEmail("email.txt")
+email = ["Matiskatibusdhw@gmail.com","sadoiv@ios12"]
+#RemoveFirstLineFile("email.txt")
+
+print(kamelObj.port)
+passwd = f'{fullName}1234'
+if changeIP:
+    proxy = requests.get('http://192.168.1.2:9049/v1/ips?num=1&country=DE&state=all&city=all&zip=all&t=txt&port=40000&isp=all&start=&end=').text.split(":")
+    IP = proxy[0]
+    port = int(proxy[1])
+else:
+    IP = "192.168.1.2"
+    port = 30000
+publicIP = requests.get("https://api.ipify.org").text
+allData = [publicIP, email, fullName, passwd]
+print(allData)
+
+if metoda == 1:
+    try:
+        kamelObj.loadProfile(f"C:\\Users\\Mati\\Desktop\\{profileName}.kameleo")
+        kamelObj.startProfile()
+    except ProblemResponseException:
+        if kamelObj.checkIDprofileByName(profileName) is not False:
+            kamelObj.setProfile(kamelObj.checkIDprofileByName(profileName))
+            kamelObj.startProfile()
+
+elif metoda == 2:
+    kamelObj.createProfile("".join(fullName.replace(" ", "")), IP, port)
     kamelObj.startProfile()
 
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option("kameleo:profileId", kamelObj.profile.id)
-    driver = webdriver.Remote(
-        command_executor=f'http://localhost:{kamelObj.port}/webdriver',
-        options=options
-    )
 
-    driver.get('https://www.nulled.to/topic/1483146-eset-nod32-antivirus-04172023/')
-    print(driver.title)
+options = webdriver.ChromeOptions()
+options.add_experimental_option("kameleo:profileId", kamelObj.profile.id)
+driver = webdriver.Remote(
+    command_executor=f'http://localhost:{kamelObj.port}/webdriver',
+    options=options
+)
 
-    time.sleep(20)
+driver.get('https://filman.cc/premium')
+print(driver.title)
 
-    kamelObj.stopProfile()
-    kamelObj.saveProfile()
-except:
-    kamelObj.stopProfile()
-    kamelObj.saveProfile()
+x = input("")
+
+kamelObj.stopProfile()
+kamelObj.saveProfile()
+
+kamelObj.startProfile()
+x = input("")
+kamelObj.stopProfile()
+kamelObj.stopClient()
+
 
